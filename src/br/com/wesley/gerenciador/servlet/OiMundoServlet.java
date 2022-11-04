9826840ca5f07424a2ddf4bcd7d5427d874563bf
Wesley Leocadio <wesley.leocadio@esig.com.br>
2022-11-03 21:43:10 -0300

    first commit

---
+++ src/br/com/wesley/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,25 @@
+package br.com.wesley.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OiMundoServlet extends HttpServlet {//
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		// TODO Auto-generated method stub
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println(" Oi mundo, parabéns vc escreveu o primeiro servlet.");
+		out.println("</body>");
+		out.println("</html>");
+
+	}
+}
